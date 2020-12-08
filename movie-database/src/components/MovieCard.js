1d0105d2c041daa0bbe1e3812b9004b6318670d5
John Melendez <john@Johns-MBP.domain>
2020-12-07 23:13:19 -0700

    added to movidCard.js

--- movie-database/src/components/MovieCard.js
+++ movie-database/src/components/MovieCard.js
@@ -2,3 +2,19 @@ import React from "react";
 import "./MovieCard.css";
 
 const Wrapper = styled((link: any));
+
+const MovieCard = (props) => (
+  <Col xs={12} sm={12} md={6} lg={6}>
+    <Wrapper to={`/movie/${props.movie.id}`}>
+      <img
+        className="card-image"
+        alt={`${props.movie.title} Movie Poster`}
+        src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`}
+      />
+      <div className="card-details">
+        <h3>{props.movie.title}</h3>
+        <p className="text-ellipsis"> {props.movie.overview} </p>
+      </div>
+    </Wrapper>
+  </Col>
+);
