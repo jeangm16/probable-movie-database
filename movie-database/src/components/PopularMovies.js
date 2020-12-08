b508c783b4c6fad99f145f280d78b8596bc6fd13
John Melendez <john@Johns-MBP.domain>
2020-12-07 22:57:36 -0700

    added movieDetails.js

--- movie-database/src/components/PopularMovies.js
+++ movie-database/src/components/PopularMovies.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react';
-import { Grid , Row } from 'react-bootstrap';
-import Search from './Search';
+import React, { Component } from "react";
+import { Grid, Row } from "react-bootstrap";
+import Search from "./Search";
+import axios from "axios";
 
 class PopularMovies extends Component {
-
-    constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-    results : []
-  };
-
+      results: [],
+    };
   }
   componentDidMount() {
-    axios.get(`${URL_DETAIL}popular${API_KEY}&language=en-US&page=1`)
-    .then((response) => {
-      this.setState({results : response.data.results});
+    axios
+      .get(`${URL_DETAIL}popular${API_KEY}&language=en-US&page=1`)
+      .then((response) => {
+        this.setState({ results: response.data.results });
       });
     document.body.style.backgroundImage = `url()`;
   }
+
+  render() {
+    const movies = this.state.results.map((movie) => (
+      <MovieCard key={movie.id} movie={movie} />
+    ));
+    return (
+      <div className="search">
+        <Search />
+        <Grid fluid={false}>
+          <Row>{movies}</Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default PopularMovies;
