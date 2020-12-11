106b1938ac74fbf09e89a7c389a3f71340821996
John Melendez <john@Johns-MBP.domain>
2020-12-11 00:12:12 -0700

    i think i fixed it again

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const config = {
+  context: __dirname,
+  entry: ["./src/index.jsx"],
+  devtool: "cheap-eval-source-map",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+    publicPath: "/src/",
+  },
+  devServer: {
+    publicPath: "/src/",
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.jsx?$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+      },
+      {
+        use: ["style-loader", "css-loader"],
+        test: /\.css$/,
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: "file-loader?name=fonts/[name].[ext]",
+      },
+    ],
+  },
+};
+
+if (process.env.NODE_ENV === "production") {
+  config.entry = "./src/index.jsx";
+  config.devtool = false;
+  config.plugins = [];
+}
+
+module.exports = config;
