064c672e1cdbd1a86c4a338ec79dfe574ecbb251
John Melendez <john@Johns-MBP.domain>
2020-12-11 00:28:33 -0700

    lets go

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import App from "./components/app.jsx";
 import "bootstrap/dist/css/bootstrap.css";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const appID = document.getElementById("app");
+const renderApp = () => {
+  render(
+    <HashRouter>
+      <App />
+    </HashRouter>,
+    appID
+  );
+};
+
+renderApp();
