0f7c4c9183da68474e01723952342ca37175b37a
John Melendez <john@Johns-MBP.domain>
2020-12-11 16:22:45 -0700

    build works

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import { render } from "react-dom";
 import { HashRouter } from "react-router-dom";
-import App from "./app";
+import App from "./components/app.js";
 
 const appID = document.getElementById("app");
 const renderApp = () => {
